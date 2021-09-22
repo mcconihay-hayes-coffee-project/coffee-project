a3f5a7758ed76de0d61ec72dfb037bbb096f2b55
Rhiannon Hayes <rhiannon.nicole.hayes@gmail.com>
2021-09-22 12:20:24 -0500

    Rhi Changes

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 "use strict"
 
+/* Displays Coffees on page  */
+
 function renderCoffee(coffee) {
     var html = '<div class="coffee">';
     html += '<div class="noId">' + coffee.id + '</div>';
@@ -10,6 +12,8 @@ function renderCoffee(coffee) {
     return html;
 }
 
+/* Loops through the coffees array  */
+
 function renderCoffees(coffees) {
     var html = '';
     for(var i = coffees.length - 1; i >= 0; i--) {
@@ -17,6 +21,8 @@ function renderCoffees(coffees) {
     }
     return html;
 }
+
+/* Bonus (Ignore for now)  */
 
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
@@ -29,6 +35,8 @@ function updateCoffees(e) {
     });
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
+
+/* Coffees Array  */
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
@@ -48,6 +56,8 @@ var coffees = [
     {id: 14, name: 'French', roast: 'dark'},
 ];
 
+/* Variables to use later, target ids  */
+
 var tbody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
 var roastSelection = document.querySelector('#roast-selection');
@@ -58,12 +68,44 @@ tbody.innerHTML = renderCoffees(coffees.reverse());
 
 submitButton.addEventListener('click', updateCoffees);
 
-// addEventListener version
-const input = document.querySelector('input[type="search"]');
 
-input.addEventListener('search', () => {
+var search = document.getElementById("search");
 
-})
+var input = document.querySelector('input[type="search"]');
 
+
+
+
+function searchInput {
+
+
+}
+
+
+
+
+/* DORIAN ADVICE
+
+***Add function to input***
+
+Make new array and store each letter in empty array.
+
+Use a loop to loop through the array.
+
+Key up and Key down.
+
+
+ */
+
+
+/* First we need to make a loop that loops through the array (we have something
+for this already).
+
+Then we need to check whatever was in the search input and compare it to
+the array.
+
+THEN we need to update the results.
+
+ */
 
 
